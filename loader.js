304a87896a0585e0f689183f174f96235c931abc
Lam M. Nguyen <30814656+lamnguyen-mltd@users.noreply.github.com>
2026-07-23 13:37:01 -0400

    Add loadComponent function to load HTML content

---
+++ loader.js
@@ -0,0 +1,8 @@
+function loadComponent(elementId, filePath) {
+  fetch(filePath)
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById(elementId).innerHTML = data;
+    })
+    .catch(error => console.error('Error loading component:', error));
+}
